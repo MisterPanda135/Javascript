92ddde4c2c7c89c3b7d2bfcbdd2d379da9d837b2
MisterPanda135 <lucadebruin@gmail.com>
2026-01-29 09:39:31 +0100

    advanced functions

--- Advanced functions/Opdracht 1/script.js
+++ Advanced functions/Opdracht 1/script.js
@@ -2,5 +2,5 @@ function toCelsius(fahrenheit) {
     return (5/9) * (fahrenheit-32);
 }
 let value1 = toCelsius(10);
-let value2 = toCelsius(1);
-console.log(typeof value1, typeof value2);
+let value2 = toCelsius;
+console.log(typeof value1, typeof value2);// displayed number function omdat value1 een nummer is en value2 een naam van een fucntion
