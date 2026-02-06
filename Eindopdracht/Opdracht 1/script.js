c1b86e699e735e70b195b860906346ca4a6ad04d
MisterPanda135 <lucadebruin@gmail.com>
2026-02-06 17:26:22 +0100

    Dobbelstenen spel Javascript bijna af

--- Eindopdracht/Opdracht 1/script.js
+++ Eindopdracht/Opdracht 1/script.js
@@ -1,5 +1,27 @@
-const getallen = [1, 2, 3, 4, 5];
+let button = document.querySelector("#button");
 
-const getSum = (totaal, nummer) => totaal + nummer;
+let textElement = document.querySelector("#text");
+let textElement2 = document.querySelector("#text2")
 
-console.log(getallen.reduce(getSum));
+button.addEventListener("click", () => {
+    waardes();
+});
+
+let waardes = () => {
+    const arr1 = [0, 0, 0, 0, 0, 0];
+    const arr2 = [0, 0, 0, 0, 0, 0];
+    const arr3 = [0, 0, 0, 0, 0, 0];
+    const arr4 = [0, 0, 0, 0, 0, 0];
+    const all = [arr1, arr2, arr3, arr4];
+    
+    const test = all.map(arr => arr.map(x => (Math.floor(Math.random() * 6) + 1)));
+    
+    total = test.map(arr => arr.reduce((sum, n) => sum + n, 0));
+    player1 = total[1] + total[0];
+    player2 = total[2] + total[3];
+    if (player1 > player2){
+        console.log("Player 1 has " + player1 + "points");
+        console.log("Player 2 has " + player2 + "points");
+        console.log("Player 1 wins!");
+    }
+}
---
+++ Eindopdracht/Opdracht 2/script.js
@@ -0,0 +1,46 @@
+let button = document.querySelector("#button");
+
+let textElement = document.querySelector("#text");
+let textElement2 = document.querySelector("#text2");
+let textElement3 = document.querySelector("#text3");
+let player1Wins = 0;
+let player2Wins = 0;
+
+const getSum = (totaal, nummer) => totaal + nummer;
+
+button.addEventListener("click", () => {
+    waardes();
+});
+
+let waardes = () => {
+    const dobbelstenen = [
+        [0],
+        [0],
+        [0],
+        [0],
+    ];
+    
+    dobbelstenenAamtal = dobbelstenen.map(arr => arr.map(() => (Math.floor(Math.random() * 6) + 1)));
+    
+    p1 = dobbelstenenAamtal.slice(0, 2);
+    p2 = dobbelstenenAamtal.slice(2);
+    
+    player1 = p1.flat();
+    player2 = p2.flat();
+
+    player1Total = player1.reduce(getSum);
+    player2Total = player2.reduce(getSum);
+    console.log("Player 1: " + player1Total);
+    console.log("Player 2: " + player2Total);
+
+    if (player1Total > player2Total){
+        textElement3.textContent = "Player 1 wins!";
+        player1Wins++
+    }if (player1Total < player2Total){
+        textElement3.textContent = "Player 2 wins!";
+    }if (player1Total == player2Total) {
+        textElement3.textContent = "Its a draw.";
+    }
+    textElement.textContent = dobbelstenenAamtal[0] + ", " + dobbelstenenAamtal[1] + "\n" + " Player1 wins:" + player1Wins;
+    textElement2.textContent = dobbelstenenAamtal[2] + ", " + dobbelstenenAamtal[3]+ "\n" + " Player2 wins:" + player2Wins;;
+}
